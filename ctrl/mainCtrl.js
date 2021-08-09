3058a38be9fddb91f404667f73d6495763d86f7b
idan ben shimon <studioidan@gmail.com>
2021-08-09 12:11:56 +0300

    local ip

--- ctrl/mainCtrl.js
+++ ctrl/mainCtrl.js
@@ -7,8 +7,8 @@ const utils = require('../utils');
 const config = require('../config');
 const Foscam = require('foscam-client');
 
- const BASE_ADDRESS = 'http://52.91.135.65';
-// const BASE_ADDRESS = 'http://192.168.1.56';
+ // const BASE_ADDRESS = 'http://52.91.135.65';
+const BASE_ADDRESS = 'http://192.168.1.56';
 
 
 const PORT = '5000';
